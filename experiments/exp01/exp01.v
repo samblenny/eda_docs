40d8bb8faba9fd051303d167a011c432960f8ed0
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2021-06-11 08:48:04 +0000

    exp01: add PLL and .pcf

--- experiments/exp01/exp01.v
+++ experiments/exp01/exp01.v
@@ -1,17 +1,29 @@
 // verilog-mode: https://veripool.org/verilog-mode/help/
+`include "pll.v"
 
 module exp01(/*AUTOARG*/
    // Outputs
-   out_1, out_2,
+   LED_RED, LED_GREEN, LED_BLUE,
    // Inputs
-   in_1, in_2, in_3
+   IOT_CLK
    );
-   input in_1, in_2, in_3;
-   output out_1, out_2;   
+   input IOT_CLK;
+   output reg LED_RED, LED_GREEN, LED_BLUE;
+   reg [14:0] count = 0;
+   wire pll_clk_out, pll_locked, clk;
    /*AUTOINPUT*/
    /*AUTOOUTPUT*/
    /*AUTOWIRE*/
    /*AUTOREG*/
-   assign out_1 = in_1 & in_2 & in_3;
-   assign out_2 = in_1 | in_2 | in_3;
+
+   pll pll_ (.clock_in(IOT_CLK), .clock_out(pll_clk_out), .locked(pll_locked));
+   SB_GB clk_gb (.USER_SIGNAL_TO_GLOBAL_BUFFER(pll_clk_out), .GLOBAL_BUFFER_OUTPUT(clk));
+
+   always @(posedge clk /*AS*/) begin
+      count <= count + 1;
+      LED_RED <= count[15] & (count[1] & count[0]);
+      LED_GREEN <= count[14] & (count[1] ^ count[0]);
+      LED_BLUE <= count[13] & ~(count[1] & count[0]);
+   end
+
 endmodule
